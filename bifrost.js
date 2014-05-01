4291a309abdbb2df85d5b106bf8796212b5648c6
Max Irwin <max_irwin@yahoo.com>
2014-05-01 11:24:43 -0400

    Placeholder

---
+++ bifrost.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+/****************************************************************************
+*
+* Bifrost - a client generator for Heimdall APIs 
+* (c)Copyright 2014, Max Iwin
+* MIT License
+*
+****************************************************************************/
+
+//Module dependencies
+var http = require('http');
+var cli = require('cli');
+var fs = require('fs');
+var _ = require('underscore');
+
+console.log('Coming Soon!');
