45aa588a2a67b27d8709ebdae26a73817f6189ba
Max Irwin <max_irwin@yahoo.com>
2014-09-27 11:01:26 +0100

    Repurposed

--- README.md
+++ README.md
@@ -1,61 +1,96 @@
-Bifrost
-=======
+# Balder
 
-Client library generator for Heimdall APIs
+State manager for mobile and web applications
 
-<img src="https://raw.githubusercontent.com/binarymax/bifrost/master/underconstruction.gif"> 
+## Intro
 
-_Currently under construction_
+Balder is a small library that focuses local and remote state management to a single location.  Balder is useful in mobile applications that make use of a local storage, and need to sync to a remote server.  Applications subscribe to local state events, and when requests are made to the remote server, the local state is synchronised, updating the application.
 
+## Todo List Conceptual Example
 
-## Command Line 
+When a new item is added to a todo list, the local state is updated.  If the device is online, the state will sync to the server.  If the device is offline, the sync will queue until the connection is enabled.  When the remote sync completes, the local state will update with any remote keys set by the server.
 
-### Installation 
+## React
 
-```
-npm install -g bifrost
-```
+A reactMixin method is provided to allow easy integration with React components.  By invoking the mixin, the component will bind to the local state.
 
-### Usage
+## Quick Start
 
-Executing bifrost will generate the source code client library that consumes a Heimdall API.  All that is needed is the url of the API documentation, and the type of library to generate. 
+To use Balder, include the script on your page:
 
-```
-bifrost [options]
-
-  Options:
-
-    -h, --help         output usage information
-    -V, --version      output the version number
-    -u, --url <url>    URL of the Heimdall API specification
-    -t, --type <type>  Type of the client to generate
-    -f, --out [file]   The output file. If missing, prints to stdout
-
-``` 
-
-### Example
-
-Executing the following command...
-```
-bifrost -u http://example.com/api -t javascript -f todoclient.js
-```
-...will generate the file ```todoclient.js```, that can be used as an AJAX library to work with the API at http://example.com/
-
-
-## Module
-
-### Installation
-```
-npm install bifrost
+```html
+<script type="text/javascript" src="javascripts/balder.js"></script>
 ```
 
-### Usage
-```
-var bifrost = require('bifrost');
-var fs = require('fs');
+In your Application, create a Balder store (where MyApp is your global app namespace):
 
-bifrost.create('http://example.com/api','javascript',function(code){
-	fs.writeFileSync(code,'todoclient.js','utf8');
+```js
+MyApp.todoStore = Balder.create("https://example.com","/todos","todoid","tododate");
+```
+
+In your List React component, add the Mixin:
+
+```jsx
+var TodoList = React.createClass({
+	mixins: [MyApp.todoStore.reactMixin()],
+	render: function() {
+		var self = this;
+		var todoitems = self.state.items.map(function(item){
+			return (
+				<TodoItem key={item.todoid} tododate={item.tododate} text={item.todotext} isdone={item.isdone} />
+			)
+		})
+		return (
+			<ul className="todo-list">
+				{todoitems}
+			</ul>
+		)
+	}
 });
 ```
 
+Create your data-entry Component, and you're done!
+
+```jsx
+var TodoEntry = React.createClass({
+	getDefaultProps:function(){
+		return {
+			"todoid": -1,
+			"todotext": "",
+			"tododate": null,
+			"isdone": false,
+		}
+	},
+	getInitialState:function(){
+		return this.getDefaultProps();
+	},
+	componentDidMount:function(){
+		var self = this;
+		Balder.on("localtodos",function(){
+			self.setState(self.getDefaultProps());
+		});
+	},
+	handleChange:function(e){
+		var self = this;
+		var state = self.state;
+		state.todotext = e.target.value;
+		self.setState(state);
+	},
+	handleSave:function(e){
+		var now = moment();
+		now.utc();
+		this.state.tododate = now.format();
+		MyApp.todoStore.add(this.state);
+	},
+	render: function(){
+		var self = this;
+		var state = self.state;
+		return (
+			<div className="todo-form">
+				<textarea id="todotext" name="todo" className="todo-text" value={state.todotext} onChange={this.handleChange} />
+				<button id="todosave" className="todo-save" onClick={this.handleSave}></button>
+			</div>
+		);
+	}
+});
+```
--- bifrost.js
+++ bifrost.js
@@ -1,13 +1,291 @@
-#!/usr/bin/env node
+var Balder = (function(global){
 
-/****************************************************************************
-*
-* Bifrost - a client generator for Heimdall APIs 
-* (c)Copyright 2014, Max Iwin
-* MIT License
-*
-****************************************************************************/
+	var _online_ = true;
 
-(require.main === module)
-	? require('./command') 					// <-- run from the command line
-	: module.exports = require('./index');	// <-- required as a node module
+	// ----------------------------------------
+	// Events
+
+	var listeners = {};
+
+	var trigger = function(type,data) {
+		// >=IE9		
+		var event = document.createEvent('HTMLEvents');
+		event.initEvent(type, true, true);
+		event.eventName = type;
+		event.target = global;
+		event.data = data || {};
+		global.dispatchEvent(event);
+	};
+
+	var on = function(type,callback) {
+		// >=IE9
+		listeners[type] = callback;
+		global.addEventListener(type, callback, false);
+	};
+
+	var off = function(type) {
+		// >=IE9
+		global.removeEventListener(type, callback||listeners[type], false);
+		if(!callback) delete events[type];
+	};
+
+
+	// ----------------------------------------
+	// Ajax
+
+	var ajax = (function(){
+		function request(type) {
+			return function(url, data, callback) {
+				var xhr = new XMLHttpRequest()
+				var body = null;
+
+				if (typeof data === 'function') {
+					callback = data;
+					data = null;
+				}
+
+				xhr.onload = function () {
+					callback.call(xhr, null, JSON.parse(xhr.response));
+				};
+
+				xhr.onerror = function () {
+					callback.call(xhr, true);
+				};
+
+				if (type === 'GET' && data) {
+					var qs = "";
+					var ch = url.indexOf('?')<0?'?':'&';
+					for(var key in data) {
+						if(data.hasOwnProperty(key)) {
+							qs = qs + ch + key + '=' + data[key];
+							ch = '&';
+						}
+					}
+					url = url + qs;
+				}
+
+				xhr.open(type, url, true);
+				xhr.withCredentials = true;
+
+				if ((type === 'POST' || type === 'PUT') && data) {
+					body = JSON.stringify(data);
+					xhr.setRequestHeader('Content-Type', 'application/json');
+				}
+
+				xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+				xhr.send(data ? body : null);
+				return xhr;
+			};
+		}
+
+		return {
+			get:request('GET'),
+			post:request('POST'),
+			del:request('DELETE'),
+			put:request('PUT')	
+		}
+	})();
+
+	// ----------------------------------------
+	// React Components
+
+	var reactMixin = function(store) {
+		return {
+			getInitialState: function() {
+				return {items:[]};
+			},
+			componentDidMount:function(){
+				var self = this;
+				on(store.localevent,function(e,d){
+					self.state.items = store.state;
+					self.setState(self.state);
+				});
+				store.collection();
+			},
+			componentWillUnmount:function(){
+				off(store.localevent);
+			}
+		}
+	};
+
+	// ----------------------------------------
+	// Main Object
+
+	var Store = function(host,resource,keyname,timestamp) {
+
+		var self = this;
+		self._host = host;
+		self._keyname = keyname;
+		self._timestamp = timestamp;
+		self._resource = resource;
+		self._url = host+resource+"/";
+		self.localevent = "local" + resource;
+		self.remoteevent = "remote" + resource;
+		self.state = [];
+	};
+
+	Store.prototype.add = function(item) {
+		var self = this;
+		self.state.push(item);
+		setLocal(self._resource,self.state);
+		var remote = function(){
+			setRemote(self._keyname,self._url,item,function(err,res){
+				trigger(self.remoteevent,self.res);
+			});
+		};
+		if (_online_) {
+			remote();
+		} else {
+			queue.add(remote);
+		}
+		trigger(self.localevent,self.state);
+	};
+
+	Store.prototype.save = function(item) {
+		var self = this;
+		var id = item[self._keyname];
+		
+		if(!id) { add(item); return; }
+		for(var i=0;i<self.state.length;i++) {
+			var stateitem = self.state[i];
+			if (stateitem[self._keyname]===id) self.state[i] = item;
+		}
+
+		setLocal(self._resource,self.state);
+
+		var remote = function(){
+			setRemote(self._keyname,self._url+id,item,function(err,res){
+				trigger(self.remoteevent,self.res);
+			});
+		};
+
+		if (_online_) {
+			remote();
+		} else {
+			queue.add(remote);
+		}
+
+		trigger(self.localevent,self.state);
+	};
+
+	Store.prototype.collection = function(query) {
+		var self = this;
+		query = query || {};
+		var remote = function(){
+			getRemote(self._url,query,function(err,res){
+
+				if (err) {
+					trigger("error"+self._resource,err);
+
+				} else {
+					var items = res.d.results;
+					var keyname = self._keyname;
+					var timestamp = self._timestamp;
+					self.state = self.state.concat(items);
+					self.state.sort(descending(keyname,timestamp));
+					setLocal(self._resource,self.state);
+					trigger(self.localevent,self.state);
+					trigger(self.remoteevent,self.res);
+
+				}
+			});
+		};
+
+		if (_online_) {
+			remote();
+		} else {
+			queue.add(remote);
+		}
+	};
+
+	Store.prototype.reactMixin = function(){
+		return reactMixin(this);
+	};
+
+	Store.prototype.reset = function(){
+		setLocal(this._resource,[]);
+	};
+
+	// ----------------------------------------
+	// localStorage Operations
+
+	var getLocal = function(resource) {
+		var state = localStorage.getItem(resource);
+			state = (state && state.length) ? JSON.parse(state) : {};
+		return state;
+	};
+
+	var setLocal = function(resource,state) {
+		localStorage.setItem(resource,JSON.stringify(state));
+		return state;
+	};
+
+	// ----------------------------------------
+	// Remote Resource Operations
+
+	var getRemote = function(resource,query,callback) {
+		ajax.get(resource,query,callback);
+	};
+
+	var setRemote = function(keyname,resource,data,callback) {
+		if (data[keyname]===-1) delete data[keyname];
+		ajax.post(resource,data,callback);
+	};
+
+	// ----------------------------------------
+	// Sort method shortcuts
+
+	var descending = function(keyname,timestamp){
+		return function(a,b){ return a[keyname] > b[keyname] ? -1 : 1; };
+	};
+
+	var ascending = function(keyname,timestamp){
+		return function(a,b){ return a[keyname] < b[keyname] ? -1 : 1; };
+	};
+
+	// ----------------------------------------
+	// Connection state
+
+	var online = function(){
+		_online_ = true;
+		queue.run();	
+	}
+
+	var offline = function(){
+		_online_ = false;
+	}
+
+	var queue = (function(){
+		var calls = [];
+		var add  = function(callback){
+			calls.push(callback);
+		};
+
+		var run = function(){
+			while (calls.length) calls.pop()();
+		};
+
+		return {
+			add:add,
+			run:run
+		};
+	})();
+
+	// ----------------------------------------
+	// Public API
+
+	var create = function(host,resource,keyname,timestamp) {
+		return new Store(host,resource,keyname,timestamp);
+	};
+
+	return {
+		trigger:trigger,
+		on:on,
+		off:off,
+		online:online,
+		offline:offline,
+		create:create
+	};
+
+})(window);
